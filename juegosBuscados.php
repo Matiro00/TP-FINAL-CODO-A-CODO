047f3065bd6d3dec22c083513da382699d9bbb6f
Matiro <matias301020000@gmail.com>
2021-07-31 19:45:55 -0300

    Se agrego la funcion de buscar juego

--- juegosBuscados.php
+++ juegosBuscados.php
@@ -3,19 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>TIENDA XYAR</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <?php include "basic_structs/header.php"?>
-    <div class="container contenidoPrincipal ">
-        <div class="searchContainer container-underline">
+    <div class="container contenidoPrincipal">
+        <div class="container searchContainer ">
             <form action="./juegoEncontrado.php" method="get" class="form-inline searchBarGroup">
                 <input type="text" name="buscar" placeholder="Buscar.." class="searchBar__input">
                 <button type="submit" class="searchBar__submit"><img src="./img/search.svg" alt="Search" class="search_icon"></button>
             </form>
+        </div>
+        <div class="container container_search_word container-underline">
+            <div class="row">
+                <div class="col-2">
+                    <h3>
+                        BUSCANDO:
+                    </h3>
+                </div>
+                <div class="col-10">
+                    <h3 class=word_searched>
+                        <?=$_GET["buscar"];?>
+                    </h3>
+                </div>
+            </div>
         </div>
         <div class="container container-juegos">
             <div class="row">
@@ -27,7 +41,7 @@
                     die("No se encuentra una conexion: " . mysqli_connect_error());
                 }
                 else{
-                    $query = mysqli_query($conn, "SELECT * FROM `juego` ORDER BY nombre asc");
+                    $query = mysqli_query($conn, "SELECT * FROM `juego` WHERE nombre LIKE'".$_GET["buscar"]."%'");
                     $cont=0;
                     $aux=0;
                     while($juego=mysqli_fetch_array($query)){
@@ -55,6 +69,8 @@
                 }?>
             </div>
         </div>
+        <?php if(mysqli_num_rows($query)==0){
+        echo '<div class="no_results"><h4>NO HAY RESULTADOS</h4></div>';}?>
     </div>
     <?php include "basic_structs/footer.html"?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
